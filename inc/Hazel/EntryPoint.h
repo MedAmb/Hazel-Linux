9514a7a17b6bf68d1ea94d748f1a561b03dcca15
medamb <ahmedmbadimed@gmail.com>
2022-03-20 23:06:45 +0100

    under developpemnt, this commit is for storage

--- inc/Hazel/EntryPoint.h
+++ inc/Hazel/EntryPoint.h
@@ -5,6 +5,10 @@ extern Hazel::Application * Hazel::createApplication();
 int main(int argc, char ** argv)
 {
     auto ClientApp = Hazel::createApplication();
+    Hazel::Log::initLogger();
+    ::Hazel::Log::getCoreLogger()->error("hello");
+    HZ_CORE_ERROR("HELLO FROM HAZEL");
+    HZ_APP_WARN("HELLO FROM APP");
     ClientApp->run();
     delete ClientApp;
 }
---
+++ inc/Hazel/Log.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include <memory>
+
+
+namespace Hazel
+{
+    class Log
+    {
+    private:
+        static std::shared_ptr<spdlog::logger> s_coreLogger;
+        static std::shared_ptr<spdlog::logger> s_clientLogger;
+
+    public:
+        static void initLogger();
+        static inline std::shared_ptr<spdlog::logger>& getCoreLogger(void) { return s_coreLogger; }
+        static inline std::shared_ptr<spdlog::logger>& getClientLogger(void) { return s_clientLogger; }
+    };
+}
+
+#define DEBUG
+#ifdef DEBUG
+#define HZ_CORE_ERROR(format) ::Hazel::Log::getCoreLogger()->error(format);
+#define HZ_CORE_WARN(format) ::Hazel::Log::getCoreLogger()->warn(format);
+#define HZ_CORE_INFO(format) ::Hazel::Log::getCoreLogger()->info(format);
+#define HZ_APP_ERROR(format) ::Hazel::Log::getClientLogger()->error(format);
+#define HZ_APP_WARN(format) ::Hazel::Log::getClientLogger()->warn(format);
+#define HZ_APP_INFO(format) ::Hazel::Log::getClientLogger()->info(format);
+#else
+#define HZ_CORE_ERROR(format)
+#define HZ_CORE_WARN(format)
+#define HZ_CORE_INFO(format)
+#define HZ_APP_ERROR(format)
+#define HZ_APP_WARN(format)
+#define HZ_APP_INFO(format)
+#endif
